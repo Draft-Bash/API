5b5d2705fe3ca716c22e2a32fe195a39a2871b44
Stephen Feddes <stephenpfeddes@lewisu.edu>
2023-09-02 13:12:40 -0500

    Adding error monitoring.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
+const appInsights = require('applicationinsights');
+appInsights.setup('03bd38c3-e3bc-47b3-9973-ae5b4595fa02');
+appInsights.start();
+app.use(appInsights.requestHandler());
+app.use(appInsights.errorHandler());
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript Express.');
