48846fe5929926d15e9b08294ec7a0a3db295c0c
Stephen Feddes <stephenpfeddes@lewisu.edu>
2023-09-02 09:47:59 -0500

    Testing deployment.

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app = express();
 const port: number = parseInt(process.env.PORT || '3000', 10); // Use the PORT environment variable or default to 3000
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express.');
+  res.send('Hello, TypeScript Express. Cool!');
 });
 
 app.listen(port, () => {
