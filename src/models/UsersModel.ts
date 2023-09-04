462dc630c092bc8de3c7d6bd5f58a628c22810e7
Stephen Feddes <stephenpfeddes@lewisu.edu>
2023-09-03 19:43:40 -0500

    Changed bcrypt dependency.

--- src/models/UsersModel.ts
+++ src/models/UsersModel.ts
@@ -1,15 +1,26 @@
 import { Request, Response } from 'express';
 const db = require("../db");
+const bcrypt = require('bcryptjs');
 
 class UsersModel {
     public async createUser(req: Request) {
         const user = req.body;
+        const saltRounds = 10;
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const bcryptPassword = bcrypt.hashSync(user.password, salt);
 
+        const duplicateRows = await db.query("SELECT * FROM user_account WHERE username = $1 OR email = $2", [
+            user.username, user.email
+        ]);
+
+
+        if (duplicateRows.rows.length < 1) {
             await db.query("INSERT INTO user_account (username, email, password) VALUES ($1, $2, $3)", [
-            user.username, user.email, user.password
+                user.username, user.email, bcryptPassword
             ]);
-
-        return true;
+            return true;
+        }
+        return false;
     }
 }
   
