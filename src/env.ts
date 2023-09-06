b2399226cbade8e93812cfb3e18ae434c3935bef
StephenFeddes <112678486+StephenFeddes@users.noreply.github.com>
2023-09-06 17:52:06 -0500

    Create env.ts for production.

---
+++ src/env.ts
@@ -0,0 +1,9 @@
+//JWT secret
+export const JWT_SECRET = "Ch@mp!xn";
+
+// DB credentials
+export const DB_USER = "ojklqiko"
+export const DB_PASSWORD = "lbrOLmLgAkLVlpLqh1i3Dhw48qu5kw1b"
+export const DB_HOST = "fanny.db.elephantsql.com"
+export const DB_PORT = 5432
+export const DB_NAME = "ojklqiko"
