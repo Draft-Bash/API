69b64e3fafc174a757c63c453ffaa9b75fd91c91
Stephen Feddes <stephenpfeddes@lewisu.edu>
2023-09-07 12:44:38 -0500

    Modified DB credentials.

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,14 @@
 const Pool = require("pg").Pool;
 import { DB_USER, DB_NAME, DB_HOST, DB_PASSWORD, DB_PORT } from "./env";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const dbConn = new Pool({
-    user: DB_USER,
-    password: DB_PASSWORD,
-    host: DB_HOST,
-    port: DB_PORT,
-    database: DB_NAME
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 });
 
 module.exports = dbConn;
