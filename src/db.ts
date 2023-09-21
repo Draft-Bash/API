1938f53c13933ac80cbd70abb7e26d76c35bcb25
Stephen Feddes <stephenpfeddes@lewisu.edu>
2023-09-21 16:23:10 -0500

    Changed db credentials.

--- src/db.ts
+++ src/db.ts
@@ -8,7 +8,8 @@ const dbConn = new Pool({
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: JSON.parse(process.env.SSL as string)
 });
 
 module.exports = dbConn;
