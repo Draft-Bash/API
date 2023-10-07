fdb11ebf6fb44e7b961d63f5a3a0781879f05aae
Brian Feddes <brianfeddes@gmail.com>
2023-10-06 21:15:06 -0500

    Had to remove image as it could not be handled by Gmail

--- src/utils/joinEmail.ts
+++ src/utils/joinEmail.ts
@@ -1,13 +1,15 @@
 import * as nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 export async function sendJoinEmails(user_email: string) {
+  // Converting image to base64 to image can be embedded in the email
+
   const html_body = `
     <h1>Thank you for joining DraftBash Fantasy!</h1>
     <h3>Draft Date & Time:</h3>
     <p>This is a test</p>
-    <img src="cid:special" width="400">
   `;
 
   // array of emails
@@ -32,13 +34,6 @@ export async function sendJoinEmails(user_email: string) {
       to: email,
       subject: 'New logo',
       html: html_body,
-      attachments: [
-        {
-          filename: 'basketball.png',
-          path: './images/basketball.png',
-          cid: 'special',
-        },
-      ],
     });
 
     console.log("Message Sent, ID: " + info.messageId);
