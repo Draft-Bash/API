742e0662135ccad6bad84076b566df2f2b782866
StephenFeddes <feddessteve@gmail.com>
2024-02-02 22:48:43 -0600

    Testing

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,8 @@
 import express, { Request, Response } from 'express';
-import { createWebSocket } from './websocket';
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || '3000';
 import dotenv from 'dotenv';
-
-const cron = require('node-cron');
 
 dotenv.config() 
 
@@ -13,7 +10,6 @@ app.use(express.json());
 app.use(cors()); // Configures the cross site resource sharing policy
 
 app.use(express.json())
-
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript Express!');
@@ -23,20 +19,6 @@ app.get('/ping', (req, res) => {
   res.json({ message: 'Server is awake!' });
 });
 
-// Route containing endpoints related to users
-app.use("/api/v1/users", require("./routes/users"));
-
-// Route containing endpoints related to drafts
-app.use("/api/v1/drafts", require("./routes/drafts"));
-
-// Route containging endpoints related to draft invites
-app.use("/api/v1/draft-invites", require("./routes/draftInvites"));
-
-// Creates port listener
-const httpServer = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
-
-// Creates the websocket, which will listen on the same port as the API.
-// In the production environment, the port is 443 (HTTPS)
-createWebSocket(httpServer);
