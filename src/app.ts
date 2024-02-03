4630a4308c421f43f61c03a2409ac861e4b74166
StephenFeddes <feddessteve@gmail.com>
2024-02-02 23:28:44 -0600

    Testing

--- src/app.ts
+++ src/app.ts
@@ -12,21 +12,12 @@ app.use(cors()); // Configures the cross site resource sharing policy
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello, TypeScript Express!");
+    res.send("Hello, TypeScript Express!"+process.env.DB_USER);
 });
 
 app.get("/ping", (req, res) => {
     res.json({ message: "Server is awake!" });
 });
-
-// Route containing endpoints related to users
-app.use("/api/v1/users", require("./routes/users"));
-
-// Route containing endpoints related to drafts
-app.use("/api/v1/drafts", require("./routes/drafts"));
-
-// Route containging endpoints related to draft invites
-app.use("/api/v1/draft-invites", require("./routes/draftInvites"));
 
 // Creates port listener
 const httpServer = app.listen(port, () => {
